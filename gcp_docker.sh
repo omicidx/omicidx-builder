e63ba4b79f2e86eb245bf6a06b986f79ea3fc323
Sean Davis <seandavi@gmail.com>
2020-04-21 12:09:50 -0400

    add env variables to cloud script

--- gcp_docker.sh
+++ gcp_docker.sh
@@ -5,19 +5,30 @@
 # Images are preemptible, so check to see that things
 # worked.
 
+#ES_HOST=""
+#GCS_STAGING_URL=""
+#GCS_EXPORT_URL=""
+
 YMD=$1 # should look like 20200401
 MIRROR_DIR=NCBI_SRA_Mirroring_${1}_Full
 gcloud compute instances create-with-container ob-sra \
-       --preemptible \
        --machine-type n1-standard-2 \
+       --container-restart-policy='never' \
        --container-env MIRROR_DIR=$MIRROR_DIR \
+       --container-env ES_HOST=$ES_HOST \
+       --container-env GCS_STAGING_URL=$GCS_STAGING_URL \
+       --container-env GCS_EXPORT_URL=$GCS_EXPORT_URL \
        --container-image seandavi/omicidx-builder \
        --boot-disk-size 100G \
        --container-command '/bin/bash' \
        --container-arg='./sra_pipeline.sh'
-gcloud compute instances create-with-container ob \
-       --preemptible \
+gcloud compute instances create-with-container ob-biosample \
        --machine-type n1-standard-2 \
+       --container-restart-policy='never' \
+       --container-env MIRROR_DIR=$MIRROR_DIR \
+       --container-env ES_HOST=$ES_HOST \
+       --container-env GCS_STAGING_URL=$GCS_STAGING_URL \
+       --container-env GCS_EXPORT_URL=$GCS_EXPORT_URL \
        --container-image seandavi/omicidx-builder \
        --boot-disk-size 100G \
        --container-command '/bin/bash' \
